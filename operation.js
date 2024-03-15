e1d0ed29b7a21ec6742a43d6f31f8c865a319943
JhonF24 <je.franco16@ciaf.edu.co>
2024-03-14 21:00:30 -0500

    version4

---
+++ operation.js
@@ -0,0 +1,35 @@
+//operatores aritmeticos basicos 
+
+let counter = 100;
+
+// 1. suma
+
+counter = counter + 20;
+console.log(counter);
+counter += 20;
+
+console.log(counter);
+
+// 2. resta
+
+counter = counter - 20;
+console.log(counter);
+counter -= 20;
+
+console.log(counter);
+
+// 3. multiplicacion
+
+counter = counter * 20;
+console.log(counter);
+counter *= 20;
+
+console.log(counter);
+
+// 4. division
+
+counter = counter / 20;
+console.log(counter);
+counter /= 20;
+
+console.log(counter);
---
+++ variables.js
@@ -0,0 +1,61 @@
+var brand = "Mala pratica";
+let counter = 100;
+const token = "abc123"
+
+console.log(token);
+counter = 200;
+console.log(counter);
+
+// comentarios 
+
+// interpolation
+
+
+
+console.log("el valor de counter es: ", counter);
+console.log("el valor de counter es: " + counter);
+
+const persons = [{
+    "id": 1,
+    "first_name": "Anestassia",
+    "last_name": "Klimkiewich"
+  }, {
+    "id": 2,
+    "first_name": "Letisha",
+    "last_name": "Fearey"
+  }, {
+    "id": 3,
+    "first_name": "Farra",
+    "last_name": "Kidney"
+  }, {
+    "id": 4,
+    "first_name": "Gayel",
+    "last_name": "Klimp"
+  }, {
+    "id": 5,
+    "first_name": "Ellene",
+    "last_name": "Bamling"
+  }, {
+    "id": 6,
+    "first_name": "Lynsey",
+    "last_name": "MacCahey"
+  }, {
+    "id": 7,
+    "first_name": "Andy",
+    "last_name": "Megson"
+  }, {
+    "id": 8,
+    "first_name": "Robena",
+    "last_name": "Voss"
+  }, {
+    "id": 9,
+    "first_name": "Dalt",
+    "last_name": "Demare"
+  }, {
+    "id": 10,
+    "first_name": "Glennie",
+    "last_name": "Chatt"
+  }]
+
+;
+console.table(persons);
